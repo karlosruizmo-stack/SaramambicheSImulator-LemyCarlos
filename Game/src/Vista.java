5a65626b217027896e016583986b7382e6eac485
Carlos Ruiz Montesinos <karlosruizmo@gmail.com>
2026-04-20 11:11:30 +0200

    xx

--- Game/src/Vista.java
+++ Game/src/Vista.java
@@ -1,7 +1,7 @@
 public class Vista {
     public void mostrarEstado(Personaje l1, Personaje l2) {
         System.out.println("\n--- PELEA SARAMAMBICHE ---");
-        System.out.println(l1.getNombre() + " HP: " + l1.getHp());
+        System.out.println(l1.getNombre() + " HP: " + l1.getHp());// Uso de getters
         System.out.println(l2.getNombre() + " HP: " + l2.getHp());
         System.out.println("--------------------------");
     }
