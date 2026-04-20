ea964e5cf7a8fe7938c0d3dc0d68ee374243321b
Lemsito22 <lemquilca22@gmail.com>
2026-04-20 12:23:36 +0200

    ..

--- Game/src/Usuario.java
+++ Game/src/Usuario.java
@@ -1,9 +1,11 @@
+import java.time.LocalDateTime;
+
 public class Usuario {
     private String nomUser;
     private Partida partidaActual; // Solo la que se está jugando o la última
 
-    public Usuario(String nomUser, Partida partidaActual) {
+    public Usuario(String nomUser) {
         this.nomUser = nomUser;
-        this.partidaActual = new Partida(0,0,20/04/2026);
+        this.partidaActual = new Partida(0,0, LocalDateTime.now());
     }
 }
