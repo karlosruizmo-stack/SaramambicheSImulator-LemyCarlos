34d4b2c1eeea238e2fb93b7f47af2d8f098f4441
Lemsito22 <lemquilca22@gmail.com>
2026-04-20 11:06:28 +0200

    Arrays de partida listos.

--- Game/src/Controlador.java
+++ Game/src/Controlador.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,30 +8,33 @@ public class Controlador {
     private Partida partidas;
     // de array a .txt
     public  void guardarEnArchivo(ArrayList<Partida> p1, Partida p3) {
-        try (java.io.FileWriter fw = new java.io.FileWriter("usuarios.txt", false)) { // 'false' borra lo viejo y escribe lo nuevo
+        try (java.io.FileWriter fw = new java.io.FileWriter("Partida.txt", false)) { // 'false' borra lo viejo y escribe lo nuevo
             for (Partida l : p1) {
-                fw.write(l.getIdPartida() + "," + l.getPuntuación() + "," + l.getResultado()+ "," + l.getFecha() + "\n");
+                fw.write(l.getIdPartida() + "," + l.getPuntuación() + "," + l.getFecha() + "\n");
             }
         } catch (Exception e) {
             System.out.println("Error al guardar: " + e.getMessage());
         }
     }
     // de .txt al array
-//    public  void cargarDesdeArchivo(ArrayList<Partida> p2) {
-//        File archivo = new File("usuarios.txt");
-//        if (!archivo.exists()) return;
-//
-//        try (Scanner lector = new Scanner(archivo)) {
-//            while (lector.hasNextLine()) {
-//                String linea = lector.nextLine();
-//                String[] partes = linea.split(","); // Dividimos por la coma
-//                if (partes.length == 3) {
-//                    // Creamos el objeto y lo metemos al array
-//                    p2.add(new Usuario(partes[0], partes[1], Integer.parseInt(partes[2])));
-//                }
-//            }
-//        } catch (Exception e) {
-//            System.out.println("Error al cargar: " + e.getMessage());
-//        }
-//    }
+
+    public  void cargarDesdeArchivo(ArrayList<Partida> p2) {
+        File archivo = new File("partida.txt");
+        if (!archivo.exists()) return;
+        DateTimeFormatter formateador = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+        try (Scanner lector = new Scanner(archivo)) {
+            while (lector.hasNextLine()) {
+                String linea = lector.nextLine();
+                String[] partes = linea.split(","); // Dividimos por la coma
+                if (partes.length == 3) {
+                    // Creamos el objeto y lo metemos al array
+                    LocalDateTime fecha = LocalDateTime.parse(partes[2], formateador);
+                    p2.add(new Partida(Integer.parseInt(partes[0]) ,Integer.parseInt(partes[1]) , fecha));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error al cargar: " + e.getMessage());
+        }
+    }
 }
