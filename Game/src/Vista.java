8de0342ea538b3b20ae28e32af882b00c4a08762
Carlos Ruiz Montesinos <karlosruizmo@gmail.com>
2026-04-20 11:02:04 +0200

    Update Vista.java

--- Game/src/Vista.java
+++ Game/src/Vista.java
@@ -1,5 +1,5 @@
 public class Vista {
-    public void mostrarEstado(Luchador l1, Luchador l2) {
+    public void mostrarEstado(Personaje l1, Personaje l2) {
         System.out.println("\n--- PELEA SARAMAMBICHE ---");
         System.out.println(l1.getNombre() + " HP: " + l1.getHp());
         System.out.println(l2.getNombre() + " HP: " + l2.getHp());
