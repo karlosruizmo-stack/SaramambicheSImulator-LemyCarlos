c8deb724c02fa7e6d8fbeed2b8e356252ce4e038
Carlos Ruiz Montesinos <karlosruizmo@gmail.com>
2026-04-20 10:23:51 +0200

    xd

--- Game/src/Vista.java
+++ Game/src/Vista.java
@@ -1,2 +1,12 @@
-public class Vista {
+public class VistaConsola {
+    public void mostrarEstado(Luchador l1, Luchador l2) {
+        System.out.println("\n--- ESTADO DEL COMBATE ---");
+        System.out.println(l1.getNombre() + " HP: " + l1.getHp());
+        System.out.println(l2.getNombre() + " HP: " + l2.getHp());
+        System.out.println("--------------------------");
+    }
+
+    public void mostrarAtaque(String atacante, String tecnica) {
+        System.out.println("¡" + atacante + " lanza un " + tecnica + "!");
+    }
 }
