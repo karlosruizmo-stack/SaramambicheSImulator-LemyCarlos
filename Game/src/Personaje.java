9b8df97e40975dd2ab8160d310c9b70ea0fb666d
Carlos Ruiz Montesinos <karlosruizmo@gmail.com>
2026-04-20 11:00:01 +0200

    c

--- Game/src/Personaje.java
+++ Game/src/Personaje.java
@@ -1,2 +1,24 @@
 public class Personaje {
+    private String nombre;
+    private int hp;
+    private int fuerza;
+
+    public Personaje(String nombre, int hp, int fuerza, int defensa) {
+        this.nombre = nombre;
+        this.hp = hp;
+        this.fuerza = fuerza;
+
+    }
+
+    // Usamos getters y llamamos al controlador
+    public String getNombre() { return nombre; }
+    public int getHp() { return hp; }
+    public int getFuerza() { return fuerza; }
+
+    @Override
+    public String toString() {
+        return nombre + " (HP: " + hp + " | STR: " + fuerza + ")";
+    }
 }
+
+
--- Game/src/Vista.java
+++ Game/src/Vista.java
@@ -1,4 +1,4 @@
-public class VistaConsola {
+public class Vista {
     public void mostrarEstado(Luchador l1, Luchador l2) {
         System.out.println("\n--- PELEA SARAMAMBICHE ---");
         System.out.println(l1.getNombre() + " HP: " + l1.getHp());
@@ -10,3 +10,4 @@ public class VistaConsola {
         System.out.println("¡" + atacante + " lanza un " + tecnica + "!");
     }
 }
+ 
