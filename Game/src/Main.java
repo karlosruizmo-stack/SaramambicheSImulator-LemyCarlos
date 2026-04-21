b197432a27ea7e83e514dd947af53d8b4aec79f0
Carlos Ruiz Montesinos <karlosruizmo@gmail.com>
2026-04-21 12:49:09 +0200

    dad

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -2,6 +2,6 @@ public static void main(String[] args) {
     Vista vista = new Vista();
     ControladorLogs logs = new ControladorLogs(vista);
     ControladorJuego juego = new ControladorJuego(vista, logs);
-1
+
     juego.iniciarJuego();
 }
